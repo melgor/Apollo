60219d16462cec733e726043f4e8f78bf0e176de
Sandipto Neogi <sandipto.neogi@gmail.com>
2015-02-21 20:17:43 +0530

    Additional scaling transformations added Generate class list and pickle it (index of class element in list is the numeric value of class label)

--- Preprocessing_Scripts/gen_augment.py
+++ Preprocessing_Scripts/gen_augment.py
@@ -7,6 +7,8 @@ from skimage.transform import SimilarityTransform
 from skimage.transform import warp
 from skimage.transform import rotate
 from sklearn.cross_validation import train_test_split
+from sklearn.utils import shuffle
+import cPickle as pickle
 
 ###############################################################################
 
@@ -14,6 +16,8 @@ from sklearn.cross_validation import train_test_split
 FTRAIN = 'train_96x96'
 # File list directory
 FLIST = '../Apollo/Data_Set'
+# File prefix
+FPREFIX = 'train_96x96_aug_'
 # Pixel size for final image
 MAX_IMAGE_PIXEL = 96
 IMAGE_SIZE = MAX_IMAGE_PIXEL * MAX_IMAGE_PIXEL
@@ -24,13 +28,15 @@ def transform():
     """
     Tranform images in training set
     """
-    X_path_label = list()
+    X_train = list()
+    X_val = list()
     directory_names = list(set(glob.glob(os.path.join(FTRAIN, "*"))).difference(set(glob.glob(os.path.join(FTRAIN,"*.*")))))
 
     n_images = 0
     n_classes = 0
     y = list()
     classes = list()
+    files = list()
     
     for folder in directory_names:
         y.append(n_classes)
@@ -39,8 +45,14 @@ def transform():
                  # Only read in the image files
                 if fileName[-4:] != ".jpg":
                   continue
+                files.append(folder + '/' + fileName)
                 n_images += 1
         n_classes += 1
+    
+    # Determine the training and validation split
+    # Files or transformations of the files in the validation set must not be in the train set
+    # Validation set must be completely independent of the training set
+    X_train_files, X_val_files = train_test_split(files, test_size = 0.1, random_state = 0)
     
     current_class_id = 0
     
@@ -52,12 +64,33 @@ def transform():
             for fileName in fileNameDir[2]:
                 if fileName[-4:] != ".jpg":
                     continue
-                print fileName
-                #f = plt.figure(figsize=(8,2))
+                #print fileName
+                
                 image_file = "{0}{1}{2}".format(fileNameDir[0], os.sep, fileName)
                 image = sc.misc.imread(image_file)
+                
+                if (folder + '/' + fileName) in X_train_files:
+                    X_train.append(folder + '/' + fileName + ' ' + str(current_class_id))
+                else:
+                    X_val.append(folder + '/' + fileName + ' ' + str(current_class_id))
 
-                X_path_label.append(folder + '/' + fileName + ' ' + str(current_class_id))                                
+                # Scale 1 original image
+                similarity_transform = SimilarityTransform(scale = 0.75)
+                image_scaled = warp(image, similarity_transform, mode = 'wrap')
+                sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_scale1.jpg', image_scaled)
+                if (folder + '/' + fileName) in X_train_files:
+                    X_train.append(folder + '/' + fileName.split('.')[0] + '_scale1.jpg' + ' ' + str(current_class_id))
+                else:
+                    X_val.append(folder + '/' + fileName.split('.')[0] + '_scale1.jpg' + ' ' + str(current_class_id))
+
+                # Scale 2 original image
+                similarity_transform = SimilarityTransform(scale = 1.25)
+                image_scaled = warp(image, similarity_transform, mode = 'wrap')
+                sc.misc.imsave(folder + '/' + fileName.split('.')[0]  + '_scale2.jpg', image_scaled)
+                if (folder + '/' + fileName) in X_train_files:
+                    X_train.append(folder + '/' + fileName.split('.')[0] + '_scale2.jpg' + ' ' + str(current_class_id))
+                else:
+                    X_val.append(folder + '/' + fileName.split('.')[0] + '_scale2.jpg' + ' ' + str(current_class_id))
 
                 # Rotate image by intervals of 45 degrees
                 for degrees in [45, 90, 135, 180, 225, 270, 315]:
@@ -66,62 +99,162 @@ def transform():
                     image_rotated = rotate(image, degrees, mode = 'wrap')
                     image_resized = np.resize(image_rotated, (MAX_IMAGE_PIXEL, MAX_IMAGE_PIXEL))
                     sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '.jpg', image_resized)
-                    X_path_label.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '.jpg' + ' ' + str(current_class_id))
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '.jpg' + ' ' + str(current_class_id))
 
                     # Shear 1
                     affine_transform = AffineTransform(shear = 0.2)
                     image_sheared = warp(image_rotated, affine_transform, mode = 'wrap')
                     sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_sheared1.jpg', image_sheared)
-                    X_path_label.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_sheared1.jpg' + ' ' + str(current_class_id))
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_sheared1.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_sheared1.jpg' + ' ' + str(current_class_id))
+
+                    # Shear 1 Scale 1
+                    similarity_transform = SimilarityTransform(scale = 0.75)
+                    image_scaled = warp(image_sheared, similarity_transform, mode = 'wrap')
+                    sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_shear1_scale1.jpg', image_scaled)
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_shear1_scale1.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_shear1_scale1.jpg' + ' ' + str(current_class_id))
+
+                    # Shear 1 Scale 2
+                    similarity_transform = SimilarityTransform(scale = 1.25)
+                    image_scaled = warp(image_sheared, similarity_transform, mode = 'wrap')
+                    sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_shear1_scale2.jpg', image_scaled)
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_shear1_scale2.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_shear1_scale2.jpg' + ' ' + str(current_class_id))
                     
-                    # shear 2
+                    # Shear 2
                     affine_transform = AffineTransform(shear = -0.2)
                     image_sheared = warp(image_rotated, affine_transform, mode = 'wrap')
                     sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_sheared2.jpg', image_sheared)
-                    X_path_label.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_sheared2.jpg' + ' ' + str(current_class_id))
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_sheared2.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_sheared2.jpg' + ' ' + str(current_class_id))
                     
+                    # Shear 2 Scale 1
+                    similarity_transform = SimilarityTransform(scale = 0.75)
+                    image_scaled = warp(image_sheared, similarity_transform, mode = 'wrap')
+                    sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_shear2_scale1.jpg', image_scaled)
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_shear2_scale1.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_shear2_scale1.jpg' + ' ' + str(current_class_id))
+
+                    # Shear 2 Scale 2
+                    similarity_transform = SimilarityTransform(scale = 1.25)
+                    image_scaled = warp(image_sheared, similarity_transform, mode = 'wrap')
+                    sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_shear2_scale2.jpg', image_scaled)
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_shear2_scale2.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_shear2_scale2.jpg' + ' ' + str(current_class_id))
+
                     # Translate 1
                     similarity_transform = SimilarityTransform(translation = (0, 20))
                     image_translated = warp(image_rotated, similarity_transform, mode = 'wrap')
                     sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate1.jpg', image_translated)
-                    X_path_label.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate1.jpg' + ' ' + str(current_class_id))
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate1.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate1.jpg' + ' ' + str(current_class_id))
                     
                     # Translate 2
                     similarity_transform = SimilarityTransform(translation = (20, 0))
                     image_translated = warp(image_rotated, similarity_transform, mode = 'wrap')
                     sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate2.jpg', image_translated)
-                    X_path_label.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate2.jpg' + ' ' + str(current_class_id))
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate2.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate2.jpg' + ' ' + str(current_class_id))
                     
                     # Translate 3
                     similarity_transform = SimilarityTransform(translation = (-20, 0))
                     image_translated = warp(image_rotated, similarity_transform, mode = 'wrap')
                     sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate3.jpg', image_translated)
-                    X_path_label.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate3.jpg' + ' ' + str(current_class_id))
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate3.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate3.jpg' + ' ' + str(current_class_id))
                     
                     # Translate 4
                     similarity_transform = SimilarityTransform(translation = (0, -20))
                     image_translated = warp(image_rotated, similarity_transform, mode = 'wrap')
                     sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate4.jpg', image_translated)
-                    X_path_label.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate4.jpg' + ' ' + str(current_class_id))
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate4.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_translate4.jpg' + ' ' + str(current_class_id))
                     
+                    # Scale 1
+                    similarity_transform = SimilarityTransform(scale = 0.75)
+                    image_scaled = warp(image_rotated, similarity_transform, mode = 'wrap')
+                    sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_scale1.jpg', image_scaled)
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_scale1.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_scale1.jpg' + ' ' + str(current_class_id))
+
+                    # Scale 2
+                    similarity_transform = SimilarityTransform(scale = 1.25)
+                    image_scaled = warp(image_rotated, similarity_transform, mode = 'wrap')
+                    sc.misc.imsave(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_scale2.jpg', image_scaled)
+                    if (folder + '/' + fileName) in X_train_files:
+                        X_train.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_scale2.jpg' + ' ' + str(current_class_id))
+                    else:
+                        X_val.append(folder + '/' + fileName.split('.')[0] + '_' + str(degrees) + '_scale2.jpg' + ' ' + str(current_class_id))
+
         current_class_id += 1
         
-    return X_path_label
+    return X_train, X_val, X_train_files, X_val_files, files, classes
 
 ###############################################################################
 
 # Apply transforms
-X_path_label = transform()
+print "========================"
+print "Applying transformations"
+print "========================"
+X_train, X_val, X_train_files, X_val_files, files, classes = transform()
+print ""
 
-# Split into test and validation sets
-X_train, X_val = train_test_split(X_path_label, test_size = 0.1, random_state = 0)
+# Shuffle training and validation lists
+print "==================="
+print "Shuffling data sets"
+print "==================="
+X_train = shuffle(X_train, random_state = 0)
+X_val = shuffle(X_val, random_state = 0)
+print ""
 
 # Write train.txt
-f = open(FLIST + '/' + 'train.txt', 'wb')
+print "====================="
+print "Writing training list"
+print "====================="
+f = open(FLIST + '/' + FPREFIX + 'train.txt', 'wb')
 f.writelines( "%s\n" % item for item in X_train )
 f.close()
+print ""
 
 # Write val.txt
-f = open(FLIST + '/' + 'val.txt', 'wb')
+print "======================="
+print "Writing validation list"
+print "======================="
+f = open(FLIST + '/' + FPREFIX + 'val.txt', 'wb')
 f.writelines( "%s\n" % item for item in X_val)
 f.close()
+print ""
+
+# Save class labels
+print "===================="
+print "Storing class labels"
+print "===================="
+with open(FLIST + '/' + FPREFIX + 'Labels.pickle', 'wb') as f:
+    pickle.dump(classes, f)
+print ""
